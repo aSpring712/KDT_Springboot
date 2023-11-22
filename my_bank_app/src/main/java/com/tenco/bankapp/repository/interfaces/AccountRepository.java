5db1735292549fec8d03f1fe2a78b4af34a2b19c
aSpring712 <rnjsqhal2001@naver.com>
2023-11-22 16:46:01 +0900

    mapper 작성

---
+++ my_bank_app/src/main/java/com/tenco/bankapp/repository/interfaces/AccountRepository.java
@@ -0,0 +1,23 @@
+package com.tenco.bankapp.repository.interfaces;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tenco.bankapp.repository.entity.Account;
+
+@Mapper
+public interface AccountRepository {
+	
+	public int insert(Account account); // 입금
+	public int updateById(Account account); // 계좌 정보 수정
+	public int deleteById(Integer id); // 계좌 정보 삭제
+	public List<Account> findAll(); // 유저가 여러 계좌를 가짐
+	public Account findById(Integer id);
+	
+//	// 사용자의 유저 정보로 계좌 리스트 출력하는 기능
+//	public List<Account> findByUserId(Integer principalId); // 접근 주체
+//	public Account findByNumber(String number); // 계좌번호로 조회
+	
+	
+}
