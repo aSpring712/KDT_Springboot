f29af323bcb05c127d049bb86ae3e09333d14d7e
aSpring712 <rnjsqhal2001@naver.com>
2023-11-23 10:27:48 +0900

    temp.jsp 화면 띄우기

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
@@ -1,5 +1,16 @@
 package com.tenco.bankapp.controller;
 
-public class TestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller // IoC의 대상, view를 return
+public class TestController {
+	
+	// GET 방식
+	// 주소 설계 - http://localhost:80/temp-test
+	
+	@GetMapping("temp-test")
+	public String tempTest() {
+		return "temp"; 
+	}
 }
