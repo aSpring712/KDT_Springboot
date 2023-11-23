a2709c517b004e5c97f133f437cc4730632b7d30
aSpring712 <rnjsqhal2001@naver.com>
2023-11-23 10:44:15 +0900

    주소 설계

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
@@ -8,9 +8,15 @@ public class TestController {
 	
 	// GET 방식
 	// 주소 설계 - http://localhost:80/temp-test
-	
-	@GetMapping("temp-test")
+	//			endpoint - 뭘로 받겠다 적어주면 됨
+	@GetMapping("temp-test") // 앞에 / 적어주는 것이 좋음(prefix 마지막에 / 적어주었으므로 생략) 
+	// -> 사용자가 web browser나 mobile로 http 요청 시 이 메서드 실행됨 
 	public String tempTest() {
-		return "temp"; 
+		// 화면은 어떻게 찾아가는가? "/WEB-INF/view/temp.jsp"
+		// BUT, yml 파일에 prefix, suffix를 설정해 두었기 때문에 "temp"만 써주어도 됨
+		
+		// prefix: /WEB-INF/view
+		return "temp"; // 화면을 return
+		// suffix: .jsp
 	}
 }
