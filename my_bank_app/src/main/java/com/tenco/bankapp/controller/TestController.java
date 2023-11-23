4d50509f1e5481204fa2931392f5bcfda866b705
aSpring712 <rnjsqhal2001@naver.com>
2023-11-23 10:46:43 +0900

    주소 반복되는 부분 RequestMapping으로 대문 만들어주기

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
@@ -2,14 +2,16 @@ package com.tenco.bankapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller // IoC의 대상, view를 return
+@Controller // IoC(Inversion of Control, 제어의 역전)의 대상, view를 return
+@RequestMapping("/temp") // /temp/temp-test, /temp/temp-test2 .. /temp가 반복되므로 RequestMapping으로 대문 만들어주기
 public class TestController {
 	
 	// GET 방식
 	// 주소 설계 - http://localhost:80/temp-test
 	//			endpoint - 뭘로 받겠다 적어주면 됨
-	@GetMapping("temp-test") // 앞에 / 적어주는 것이 좋음(prefix 마지막에 / 적어주었으므로 생략) 
+	@GetMapping("/temp-test") // 앞에 / 적어주는 것이 좋음(prefix 마지막에 / 적어주었으므로 생략) 
 	// -> 사용자가 web browser나 mobile로 http 요청 시 이 메서드 실행됨 
 	public String tempTest() {
 		// 화면은 어떻게 찾아가는가? "/WEB-INF/view/temp.jsp"
