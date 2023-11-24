f62d9d881e088cf2174026be23da4cc6d728972c
aSpring712 <rnjsqhal2001@naver.com>
2023-11-24 12:49:44 +0900

    계좌 추가

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bankapp.dto.SaveFormDto;
 import com.tenco.bankapp.handler.exception.CustomRestfulException;
 import com.tenco.bankapp.handler.exception.UnAuthorizedException;
 import com.tenco.bankapp.repository.entity.User;
+import com.tenco.bankapp.service.AccountService;
 import com.tenco.bankapp.utils.Define;
 
 @Controller // View를 제공하는 controller로 설정
@@ -19,6 +22,9 @@ public class AccountController {
 
 	@Autowired
 	private HttpSession session;
+	
+	@Autowired
+	private AccountService accountService;
 	
 	// 임시 예외 발생 확인용 http://localhost:80/account/list
 //	@GetMapping("/list")
@@ -44,4 +50,45 @@ public class AccountController {
 		return "account/list"; // '파일 찾는 경로' 여기는 / 안붙임!
 	}
 	
+	@GetMapping("/save")
+	public String save() {
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		
+		if(principal == null) {
+			throw new UnAuthorizedException("로그인 먼저 해주세요", 
+					HttpStatus.UNAUTHORIZED);
+		}
+		
+		return "account/save";
+	}
+	
+	@PostMapping("/save")
+	public String saveProc(SaveFormDto dto) {
+		
+		// 1. 인증검사 -> 매번 반복해서 해줘야 함(필터 처리도 있지만 보다 스프링 컨테이너 내에서 인터셉터 처리하기. 스프링 시큐리티를 사용하는게 아니라면)
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		
+		if(principal == null) {
+			throw new UnAuthorizedException("로그인 먼저 해주세요", 
+					HttpStatus.UNAUTHORIZED);
+		}
+		
+		// 2. 유효성 검사
+		if(dto.getNumber() == null || dto.getNumber().isEmpty()) {
+			throw new CustomRestfulException("계좌번호를 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new CustomRestfulException("계좌 비밀번호를 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getBalance() == null || dto.getBalance() <= 0) {
+			throw new CustomRestfulException("잘못된 입력입니다", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 인증검사, 유효성 검사를 마쳤다면 생성하는 일은 Service에게 위임
+		accountService.createAccount(dto, principal.getId());
+		
+		return "account/list";
+	}
 }
---
+++ my_bank_app/src/main/java/com/tenco/bankapp/service/AccountService.java
@@ -0,0 +1,46 @@
+package com.tenco.bankapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tenco.bankapp.dto.SaveFormDto;
+import com.tenco.bankapp.handler.exception.CustomRestfulException;
+import com.tenco.bankapp.repository.entity.Account;
+import com.tenco.bankapp.repository.interfaces.AccountRepository;
+
+@Service // IoC 대상 + 싱글톤 관리
+public class AccountService {
+	
+	@Autowired
+	private AccountRepository accountRepository;
+
+	
+	/**
+	 * 계좌 생성 기능
+	 * @param dto
+	 * @param priIntegerId
+	 */
+	@Transactional // select로 계좌 중복 여부 확인 후 중복이 아니면 생성해야 하므로 -> Transactional 걸어줌. 그리고 하나의 서비스가 됨
+	public void createAccount(SaveFormDto dto, Integer priIntegerId) {
+		
+		// 계좌 중복 여부 확인
+		
+		
+		Account account = Account.builder()
+				.number(dto.getNumber())
+				.password(dto.getPassword())
+				.balance(dto.getBalance())
+				.userId(priIntegerId)
+				.build();
+		
+		
+		int resultRowCount = accountRepository.insert(account);
+		if(resultRowCount != 1) {
+			throw new CustomRestfulException("계좌 생성 실패", 
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+}
