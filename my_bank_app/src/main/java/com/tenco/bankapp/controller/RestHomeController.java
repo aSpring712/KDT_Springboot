132a7edf8dace52493d8354e40a8b6ab94dfeac4
aSpring712 <rnjsqhal2001@naver.com>
2023-11-29 14:51:19 +0900

    외부 서버에 요청 보내기(jsonplaceholder)

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/RestHomeController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/RestHomeController.java
@@ -2,8 +2,13 @@ package com.tenco.bankapp.controller;
 
 import java.net.URI;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +43,7 @@ public class RestHomeController {
 				.build()
 				.toUri(); // 이렇게 하지않고 new URI 해도 됨
 		
-		// 2. 메모리에 올리기
+		// 2. 객체 생성
 		RestTemplate restTemplate = new RestTemplate();
 		
 		// 다른 서버에 접근해서 자원 요청
@@ -49,6 +54,52 @@ public class RestHomeController {
 		System.out.println(response.getHeaders());
 		
 		// MIME TYPE : text/html;charset=UTF-8	
+		return ResponseEntity.status(HttpStatus.OK).body(response.getBody());
+	}
+	
+	// POST 방식과 exchange 메서드 사용
+	@GetMapping("/exchange-test")
+	public ResponseEntity<?> restTemplateTest2() {
+		
+		// 자원 등록 요청 --> POST 방식 사용법
+		// 1. URI 객체 만들기
+		URI uri = UriComponentsBuilder
+				.fromUriString("https://jsonplaceholder.typicode.com")
+				.path("/posts")
+				.encode()
+				.build()
+				.toUri();
+		
+		// 2. 객체 생성
+		RestTemplate restTemplate = new RestTemplate();
+		
+		// exchange 사용 방법
+		// 1. HttpHeaders 객체를 만들고 Header 메세지 구성
+		// 2. body 데이터를 key=value 구조로 만들기
+		// 3. HttpEntity 객체를 생성해서 Header와 결합 후 요청
+		
+		// 헤더 구성
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/json; charset=UTF-8");
+		
+		// 바디 구성 -> JSON 형식의 데이터를 만들어주는 아이
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("title", "블로그 포스트 1"); // 어떤 형식으로 데이터를 보낼지는 서버와의 약속
+		params.add("body", "후미진 어느 언덕에서 도시락 소풍");
+		params.add("userId", "1");
+		
+		// 헤더와 바디 결합
+		HttpEntity<MultiValueMap<String, String>> requestMessage
+			= new HttpEntity<>(params, headers);
+		//                   body,   header
+		
+		// HTTP 요청 처리 									-- 요청  POST 방식        body            응답은 String class로 받기
+		ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, requestMessage, String.class);
+		
+		// 어떻게 오는지 확인
+		// http://localhost:80/exchange-test
+		System.out.println("headers " + response.getHeaders());
+		
 		return ResponseEntity.status(HttpStatus.OK).body(response.getBody());
 	}
 	
